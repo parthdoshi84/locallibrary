db11e9cc967b829a12c8e6d2436728b15cacf032
parthdoshi84 <parthdoshi84@gmail.com>
2019-05-10 22:05:11 -0700

    Second Commit

--- UCLALibrary/MyApp/admin.py
+++ UCLALibrary/MyApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from MyApp.models import Author, Type, Book, Publisher
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Type)
+admin.site.register(Publisher)
